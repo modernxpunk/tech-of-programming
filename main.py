25ce8be64f4808e3a1950d9dee6711cd9358a8b8
modernxpunk <modernxpunk@gmail.com>
2021-10-26 12:01:06 +0300

    add report

---
+++ main.py
@@ -0,0 +1,20 @@
+class Calculator():
+	def addition(self, a, b):
+		return a + b
+
+	def subtract(self, a, b):
+		return a - b
+
+	def mult(self, a, b):
+		return a * b
+
+	def division(self, a, b):
+		return a / b
+
+
+c = Calculator()
+
+print(c.addition(1, 4))
+print(c.subtract(6, 3))
+print(c.mult(2, 3))
+print(c.division(10, 5))
